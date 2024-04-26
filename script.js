b55cf06edeac960f3d4e865eccc142d872419507
Aslan <aslan.mammadov96@gmail.com>
2024-04-26 09:08:13 -0400

    display fetched images on the screen

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
-const BASE_URL = 'https://api.giphy.com';
-const API_KEY='6FooVD77mC0C5anr03X6Lp0HjHVBTfZ6';
-const searchBtn = document.querySelector('.js-search-btn');
-let search = document.querySelector('.js-search-item');
-let quantity = document.querySelector('.js-quantity');
-let container = document.querySelector('.js-container');
+const BASE_URL = "https://api.giphy.com";
+const API_KEY = "6FooVD77mC0C5anr03X6Lp0HjHVBTfZ6";
+const searchBtn = document.querySelector(".js-search-btn");
+let search = document.querySelector(".js-search-item");
+let quantity = document.querySelector(".js-quantity");
+let container = document.querySelector(".js-container");
 
-
-
-searchBtn.addEventListener('click', async ()=>{
-    let searchValue=search.value;
-    let quantityValue = quantity.value;
-    search.value='';
-    if(searchValue==''){
-        alert('enter search value')
-        return ;
+searchBtn.addEventListener("click", async () => {
+  let searchValue = search.value;
+  let quantityValue = quantity.value;
+  search.value = "";
+  if (searchValue == "") {
+    container.innerHTML = `<h3 class='warning'>Please Enter Valid Search</h3>`;
+    return;
+  }
+  try {
+    let response = await fetch(
+      `${BASE_URL}/v1/gifs/search?api_key=${API_KEY}&q=${searchValue}&limit=${quantityValue}`
+    );
+    let responseJson = await response.json();
+    let data = responseJson.data;
+    container.innerHTML = "";
+    console.log(data.length);
+    if (data.length > 0) {
+      for (let x in data) {
+        container.innerHTML =
+          container.innerHTML + `<img src=${data[x].images.original.url}>`;
+      }
+    } else {
+      container.innerHTML = `<h3 class='warning'>Sorry. No result has found.</h3>`;
     }
-    let response = await fetch (`${BASE_URL}/v1/gifs/search?api_key=${API_KEY}&q=${searchValue}&limit=${quantityValue}`);
-    let data = await response.json();
-    console.log(data);
-    if(data.data.length>0){
-        container.innerHTML=''
-    }
-    });
+  } catch {
+    container.innerHTML += `<h3 class='warning'>No results found.</h3>`;
+  }
+});
